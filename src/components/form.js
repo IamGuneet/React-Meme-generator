5dfc5245639838d15b4e506b5d124ad9e3c96131
Guneet <guneet.iam@gmail.com>
2022-11-05 14:38:18 +0530

    basic components , css added

---
+++ src/components/form.js
@@ -0,0 +1,19 @@
+
+export default function form() {
+    return (
+        <div >
+            <form className="form">
+                <input className="formInput" 
+                type="text"
+
+                />
+                <input  className="formInput"
+                type="text"
+
+                />
+                <button className="formButton">Get a new Meme</button>
+            </form>
+        </div>
+    )
+    
+};
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import Logo from "../imgs/Albert.png"
+
+
+export default function navbar() {
+    return (
+        <div className="navbar">
+            <img src={Logo} alt="Img" className="logo"/>
+            <h1>Meme Generator</h1>
+        
+        </div>
+    )
+    
+};
