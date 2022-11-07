73ca02ffde4732fb3c4b6aa49b2769323fdaa40d
Guneet <guneet.iam@gmail.com>
2022-11-07 18:39:41 +0530

    object input state added

--- src/components/form.js
+++ src/components/form.js
@@ -3,10 +3,27 @@ import React from "react"
 
 export default function Form() {
 
+    const [formData,setFormData] = React.useState(
+        {firstname:"",lastName:""}
+    );
+    console.log(formData);
+    
     const [url,setUrl] = React.useState("https://i.imgflip.com/30b1gx.jpg") 
-
+    
     const memeFunc = dataObj()
     const memeArray = memeFunc.data.memes 
+    
+    function handleChange(e){
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [e.target.name]: e.target.value
+            }
+        })
+        console.log(e.target.name);
+    }
+
+
     function getRandomMeme(){
         const randNumber = Math.floor(Math.random() *memeArray.length);
         console.log(randNumber);
@@ -18,12 +35,17 @@ export default function Form() {
             <div className="form">
                 <input className="formInput" 
                 type="text"
+                onChange={handleChange}
                 placeholder="Top text"
-
+                name="firstname"      
+                value={formData.firstname}
                 />
                 <input  className="formInput"
                 type="text"
                 placeholder="Bottom text"
+                onChange={handleChange}
+                name="lastname"
+                value={formData.lastName}
                 />
                 <button onClick={getRandomMeme} className="formButton">Get a new Meme</button>
             </div>
