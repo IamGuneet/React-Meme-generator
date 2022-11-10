e84cf515024579d72be8cb529d76a37447046d4a
Guneet <guneet.iam@gmail.com>
2022-11-10 18:28:15 +0530

    done

--- src/components/form.js
+++ src/components/form.js
@@ -1,51 +1,48 @@
 import React from "react"
-// import dataObj from "../Data/memeData"
 
 export default function Form() {
 
     const [formData,setFormData] = React.useState(
         {firstName:"",lastName:""}
     );
-    console.log(formData);
     
     const[randomNumber,setRandomNumber]= React.useState(0);
     
     const [url,setUrl] = React.useState("") 
     
         React.useEffect(()=>{
+    
             fetch(`https://api.imgflip.com/get_memes`)
             .then(res => res.json())
             .then(obj => setUrl(obj.data.memes[randomNumber].url))
+    
         },[randomNumber])
 
 
-    // const memeFunc = dataObj()
-    // const memeArray = memeFunc.data.memes 
     
     function handleChange(e){
+    
         setFormData(prevFormData => {
             return {
                 ...prevFormData,
                 [e.target.name]: e.target.value
             }
         })
-        console.log(e.target.name);
+    
     }
 
 
     function getRandomMeme(){
+
         const randNumber = Math.floor(Math.random() *99);
-        // console.log(randNumber);
         setRandomNumber(randNumber);
-        // setUrl(memeArray[randomNumber].url);
+
     }
 
     return (
         
         <div className="memeDiv" >
-
             <div className="form">
-
                 <input className="formInput" 
                 type="text"
                 onChange={handleChange}
@@ -69,7 +66,8 @@ export default function Form() {
                 <img src={url} alt="meme" className="memeImg"/>
                 <h1 className="meme-topText">{formData.firstName} </h1>
                 <h1 className="meme-bottomText">{formData.lastName}</h1>
-                
+
+
                 </div>
                 
         </div>
